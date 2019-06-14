0236ac765280f843f3645f40d48b2b9738cbd4c6
Mondotrasho <oscar@peoplepictures.com.au>
2019-06-14 14:22:11 +1000

    Added stats to pieces

---
+++ Assets/HexStats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexStats : MonoBehaviour
+{
+    public int Range = 1;
+    public int HP = 1;
+    public int DMG = 1;
+    public int total_AP = 2;
+    public int remaining_AP = 2;
+    public bool Knight = false;
+
+}
